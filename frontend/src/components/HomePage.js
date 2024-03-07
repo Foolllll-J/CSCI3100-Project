bea5366e6b1436a8c84c7fda5034566ebe0eb487
Yitiao <1912468701@qq.com>
2024-03-08 02:25:21 +0800

    1

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -45,6 +45,7 @@ function Home() {
             <h1>This is a Home page.</h1>
             <h1>This is a Home page.</h1>
             <h1>This is a Home page.</h1>
+            <h1>This is a Home page.</h1>
         </div>
     )
 }
