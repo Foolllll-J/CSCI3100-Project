3e34ef99c524d3ccd0f723fcdfa98a99290e272b
Yitiao <1912468701@qq.com>
2024-03-08 03:18:00 +0800

    1

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,11 +1,13 @@
 import '../App.css'
 
+// Todo: 触底函数
+
 function Home() {
     return (
-        <div className='main-container' style={{top: '14vh'}}>
+        <div className='main-container' style={{top: '11vh'}}>
             <div className='home-nav'>
                 <div className='home-nav-logo-container'>
-                    <img src='Full-Cart.svg' style={{height: '90%', width: '100%'}}></img>
+                    <img src='../assets/logo.png' alt='Logo' style={{height: '50px', width: '125px', position: 'fixed', left: '3vw'}} />
                 </div>
                 <div className='home-nav-category-container'>
                     <div className='home-nav-category-cell'>class1</div>
@@ -15,37 +17,22 @@ function Home() {
                     <div className='home-nav-category-cell'>More...</div>
                 </div>
                 <div className='home-nav-searchbox-container'>
-                    <div class="input-group flex-nowrap" style={{width: '15vw', position: 'fixed', right: '3vw'}}>
-                        <span class="input-group-text" id="addon-wrapping">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                    <div className="input-group flex-nowrap" style={{width: '15vw', position: 'fixed', right: '3vw'}}>
+                        <span className="input-group-text" id="addon-wrapping">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
                                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
                             </svg>
                         </span>
-                        <input type="text" class="form-control" placeholder="Search..." aria-describedby="addon-wrapping" />
+                        <input type="text" className="form-control" placeholder="Search..." aria-describedby="addon-wrapping" />
                     </div>
                 </div>
             </div>
-            <h1>This is a Home page.</h1>
-            <h1>This is a Home page.</h1>
-            <h1>This is a Home page.</h1>
-            <h1>This is a Home page.</h1>
-            <h1>This is a Home page.</h1>
-            <h1>This is a Home page.</h1>
-            <h1>This is a Home page.</h1>
-            <h1>This is a Home page.</h1>
-            <h1>This is a Home page.</h1>
-            <h1>This is a Home page.</h1>
-            <h1>This is a Home page.</h1>
-            <h1>This is a Home page.</h1>
-            <h1>This is a Home page.</h1>
-            <h1>This is a Home page.</h1>
-            <h1>This is a Home page.</h1>
-            <h1>This is a Home page.</h1>
-            <h1>This is a Home page.</h1>
-            <h1>This is a Home page.</h1>
-            <h1>This is a Home page.</h1>
-            <h1>This is a Home page.</h1>
-            <h1>This is a Home page.</h1>
+            <div style={{textAlign: 'center', marginTop: '3vh'}}>
+                <h1>Recommendations</h1>
+            </div>
+            <div className='home-products-container'>
+
+            </div>
         </div>
     )
 }
